5fe8c2babadca3ffaa1ac07977d6fd3b7715fa35
kristaps775 <b349749@gmail.com>
2024-12-22 18:49:59 +0200

    initial commit

---
+++ kt23030_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from kt23030_libadd import *
+from kt23030_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
